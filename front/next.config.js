652c77289cc6694835748288a4cdb2568c43fa23
DashaGamarska <108406951+DashaGamarska@users.noreply.github.com>
2024-06-06 23:25:19 +0300

    Update next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  BASE_URL = 'https://full-project2.onrender.com'
+    env: {
+    BASE_URL: 'https://full-project2.onrender.com',
+  },
   images: {
     domains: ['i.ibb.co'],
     remotePatterns: [
